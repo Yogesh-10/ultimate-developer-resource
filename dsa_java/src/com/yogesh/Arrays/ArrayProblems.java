1664b69b67874e394db67c88feab243a2829176a
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-05-22 23:19:12 +0530

    Array problems and solutions

--- dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
+++ dsa_java/src/com/yogesh/Arrays/ArrayProblems.java
@@ -45,8 +45,8 @@ public class ArrayProblems {
     }
 
     //4. Reverse an array
-    public static int[] reverseArray(int[] arr){
-        for (int i = arr.length - 1, j = 0; i > j ; j++, i--) {
+    public static int[] reverseArray(int[] arr) {
+        for (int i = arr.length - 1, j = 0; i > j; j++, i--) {
             int temp = arr[j];
             arr[j] = arr[i];
             arr[i] = temp;
@@ -55,10 +55,10 @@ public class ArrayProblems {
     }
 
     //5. remove duplicates from sorted array
-    public static int removeDuplicates(int[] arr){
+    public static int removeDuplicates(int[] arr) {
         int size = arr.length;
 
-        for (int i = 0; i < arr.length - 1; i++){
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] == arr[i]) {
                 arr[i] = arr[i + 1];
                 size--;
@@ -69,11 +69,11 @@ public class ArrayProblems {
     }
 
     //6. Move zeros to end.
-    public static int[] moveZerosToEnd(int[] arr){
+    public static int[] moveZerosToEnd(int[] arr) {
         int count = 0;
 
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] != 0){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
                 //swap zero and non-zero number
                 int temp = arr[count];
                 arr[count] = arr[i];
@@ -83,5 +83,104 @@ public class ArrayProblems {
             }
         }
         return arr;
+    }
+
+    //7. Left Rotate Array.
+    public static int[] leftRotate(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = temp;
+        }
+
+        return arr;
+    }
+
+    public static int[] leftRotateByDTimes(int[] arr, int d) {
+        //solution 1 - O(nd) time, Aux space - O(1)
+//        for (int i = 0; i < d; i++) {
+//            leftRotate(arr);
+//        }
+//        System.out.println(Arrays.toString(arr));
+
+        //solution 2 - O(n) time, Aux space - O(d)
+//        int[] temp = new int[d];
+//        for (int i = 0; i < d; i++)
+//            temp[i] = arr[i];
+//        for (int i = d; i < arr.length; i++)
+//            arr[i - d] = arr[i];
+//        for (int i = 0; i < d; i++)
+//            arr[arr.length - d + i] = temp[i];
+//        return arr;
+
+        //solution-3 O(n) time and O(1) space
+
+        //reverse the d elements
+        reverse(arr, 0, d - 1);
+        //reverse the remaining elements
+        reverse(arr, d, arr.length - 1);
+        //reverse entire array
+        reverse(arr, 0, arr.length - 1);
+
+        return arr;
+    }
+
+    private static void reverse(int[] arr, int low, int high){
+        while (low < high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    public static void leaderInArray(int[] arr) {
+        //7,10,4,3,6,5,2
+        //O(n^2)
+//        for (int i = 0; i < arr.length; i++){
+//            boolean flag = false;
+//            for (int j = i + 1; j < arr.length; j++){
+//                if (arr[i] <= arr[j]) {
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//            if (!flag){
+//                System.out.print(arr[i] + " ");
+//            }
+//        }
+
+        //O(n) - solution
+        int currLeader = arr[arr.length - 1];
+        System.out.print(currLeader + " ");
+
+        for (int i = arr.length - 2; i >= 0; i--){
+            if (currLeader < arr[i]) {
+                currLeader = arr[i];
+                System.out.print(currLeader + " ");
+            }
+        }
+    }
+
+    public static int maximumDifference(int[] arr){
+//        O(n ^ 2) solution,  Aux Space - O(1)
+//        int max = arr[1] - arr[0];
+//        for (int i = 0; i < arr.length; i++){
+//            for (int j = i + 1; j < arr.length; j++){
+//                max = Math.max(max, arr[j] - arr[i]);
+//            }
+//        }
+//        System.out.println(max);
+
+        //O(n ^ 2) solution, Aux Space - O(1)
+        int minValue = arr[0];
+        int res = arr[1] - arr[0];
+
+        for (int i = 1; i < arr.length; i++){
+            res = Math.max(res, arr[i] - minValue);
+            minValue = Math.min(minValue, arr[i]);
+        }
+        return res;
     }
 }
